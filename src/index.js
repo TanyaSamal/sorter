c438754bd9fb41a600465bcb1e9b52b87769392a
TanyaSamal <solesytto@gmail.com>
2018-09-13 23:38:44 +0300

    resolve sorter

--- src/index.js
+++ src/index.js
@@ -1,31 +1,49 @@
 class Sorter {
+
   constructor() {
-    // your implementation
+    this.elements = [];
+    this.comparator = (left, right) => left - right;
   }
 
   add(element) {
-    // your implementation
+    this.elements.push(element);
   }
 
   at(index) {
-    // your implementation
+    return this.elements[index];
   }
 
   get length() {
-    // your implementation
+    return this.elements.length;
   }
 
   toArray() {
-    // your implementation
+    return this.elements;
   }
 
   sort(indices) {
-    // your implementation
+    let arrTemp = [];
+    indices.sort(function(a, b) {
+      return a - b;
+    });
+
+    for (let value of indices){
+      arrTemp.push(this.elements[value]);
+    }
+
+    arrTemp.sort(this.comparator);
+
+    let i = 0;
+    for (let value of indices){
+      this.elements[value] = arrTemp[i];
+      i++;
+    }
   }
 
   setComparator(compareFunction) {
-    // your implementation
+    this.comparator = compareFunction;
   }
 }
+
 
 module.exports = Sorter;
